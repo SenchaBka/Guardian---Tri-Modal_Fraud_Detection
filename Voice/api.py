ed7e55098add51e210ec86faa968ee9230e40ddd
Arsenii Buriak <128735775+SenchaBka@users.noreply.github.com>
2026-02-22 16:19:30 -0500

    embedding size fix

--- Voice/api.py
+++ Voice/api.py
@@ -68,9 +68,10 @@ def wavlm_embedding(audio_1d: np.ndarray) -> np.ndarray:
     inputs = feature_extractor(audio_1d, sampling_rate=16000, return_tensors="pt")
     inputs = {k: v.to(DEVICE) for k, v in inputs.items()}
     hidden = wavlm(**inputs).last_hidden_state  # (B,T,D)
-    mean = hidden.mean(dim=1)
-    std = hidden.std(dim=1)
-    emb = torch.cat([mean, std], dim=1).squeeze(0)
+    #mean = hidden.mean(dim=1)
+    #std = hidden.std(dim=1)
+    #emb = torch.cat([mean, std], dim=1).squeeze(0)
+    emb = hidden.mean(dim=1).squeeze(0)
     return emb.detach().cpu().numpy().astype(np.float32)
 
 # Audio loader
