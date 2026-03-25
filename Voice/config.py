e6d53a40705c97a37de53cfaef1f59a8cec5fc18
Arsenii Buriak <128735775+SenchaBka@users.noreply.github.com>
2026-03-25 17:29:03 -0400

    Use large model instead of base

--- Voice/config.py
+++ Voice/config.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import torch
 
-MODEL_NAME = "microsoft/wavlm-base"
+MODEL_NAME = "microsoft/wavlm-large"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-CLF_PATH = Path("data/ASV/cache/wavlm_lr.joblib")
-THR_PATH = Path("data/ASV/cache/threshold.txt")
+MODEL_PATH = Path("data/ASV/cache/wavlm_lr.joblib")
+THRESHOLD_PATH = Path("data/ASV/cache/threshold.txt")
 
 MAX_UPLOAD_MB = 30
