052eccfa1d03cc8f2d48d0c13168f8586e1cdcac
Arsenii Buriak <128735775+SenchaBka@users.noreply.github.com>
2026-03-26 15:16:08 -0400

    imports fix

--- Voice/model_loader.py
+++ Voice/model_loader.py
@@ -1,21 +1,21 @@
-from config import MODEL_NAME, DEVICE, CLF_PATH, THR_PATH
+from config import MODEL_NAME, DEVICE, MODEL_PATH, THRESHOLD_PATH
 import joblib
 from transformers import AutoFeatureExtractor, AutoModel
 from fastapi import HTTPException
 
 # Load classifier
 try:
-    clf = joblib.load(CLF_PATH)
+    clf = joblib.load(MODEL_PATH)
 except Exception as e:
     raise HTTPException(status_code=500, detail=f"Model load failed: {e}")
 
 # Load threshold
 def load_threshold() -> float:
-    if not THR_PATH.exists():
+    if not THRESHOLD_PATH.exists():
         raise HTTPException(status_code=500, detail="Threshold file missing")
 
     try:
-        return float(THR_PATH.read_text(encoding="utf-8").strip())
+        return float(THRESHOLD_PATH.read_text(encoding="utf-8").strip())
     except:
         raise HTTPException(status_code=500, detail="Invalid threshold file")
 
